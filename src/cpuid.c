4d33213b1c0e3a3c2f1c3d165d106d5dfab2df70
Pireax <pim.remkes@gmail.com>
2017-07-14 14:07:57 +0200

    MSVC support

--- src/cpuid.c
+++ src/cpuid.c
@@ -1,7 +1,13 @@
 #include <stdint.h>
 
 void c_cpuid(uint32_t* a, uint32_t* b, uint32_t* c, uint32_t* d) {
+#ifdef _MSC_VER
+   uint32_t regs[4];
+   __cpuidex((int*)regs, *a, *c);
+   *a = regs[0], *b = regs[1], *c = regs[2], *d = regs[3];
+#else
    asm volatile ("cpuid"
       : "+a"(*a), "=b"(*b), "+c"(*c), "=d"(*d)
    );
+#endif
 }
