dc35c51831831604c6c9978b908c8bf7a3f72e90
Pireax <pim.remkes@gmail.com>
2017-07-16 05:05:30 +0200

    rust inline assembly feature

--- build.rs
+++ build.rs
@@ -1,5 +1,11 @@
 extern crate gcc;
 
+#[cfg(not(feature = "nightly"))]
 fn main() {
     gcc::compile_library("libcpuid.a", &["src/cpuid.c"]);
+}
+
+#[cfg(feature = "nightly")]
+fn main() {
+    
 }
