gz/rust-cpuid

commit cbd64dcfa4248c1b2c8c0a044af7e0c86a0c6f39

Consider the terminating NULL character of the extended brand string instead of always going for the 48 characters.
See Intel Vol. 2A, Table 3.13.
Fixes question mark characters in extended brand string (e.g. when using raw_cpuid under QEMU).

src/lib.rs
@@ -2788,7 +2788,13 @@ impl ExtendedFunctionInfo {
         if self.leaf_is_supported(EAX_EXTENDED_BRAND_STRING) {
             Some(unsafe {
                 let brand_string_start = transmute::<&CpuIdResult, *const u8>(&self.data[2]);
-                let slice = slice::from_raw_parts(brand_string_start, 3 * 4 * 4);
+                let mut slice = slice::from_raw_parts(brand_string_start, 3 * 4 * 4);
+
+                match slice.iter().position(|&x| x == 0) {
+                    Some(index) => slice = slice::from_raw_parts(brand_string_start, index),
+                    None => (),
+                }
+
                 let byte_array: &'static [u8] = transmute(slice);
                 str::from_utf8_unchecked(byte_array)
             })

commit 04bc134748d843a50cc91cdf6eb047ca5e4d17b2

Remove null character from test.
This was broken in cbd64dcfa4248c1b2c8c0a044af7e0c86a0c6f39.

src/lib.rs
@@ -3510,7 +3510,7 @@ fn extended_functions() {
     };
 
     assert_eq!(ef.processor_brand_string().unwrap(),
-               "       Intel(R) Core(TM) i5-3337U CPU @ 1.80GHz\0");
+               "       Intel(R) Core(TM) i5-3337U CPU @ 1.80GHz");
     assert!(ef.has_lahf_sahf());
     assert!(!ef.has_lzcnt());
     assert!(!ef.has_prefetchw());