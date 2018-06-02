1604e3fda914fc4b07270f47468e3abfef0c8c1f
Gabriel Majeri <gabriel.majeri6@gmail.com>
2018-06-02 08:54:18 +0300

    Automatically detect the Rust version

--- build.rs
+++ build.rs
@@ -1,9 +1,16 @@
+extern crate rustc_version;
 extern crate cc;
+use rustc_version::{version, version_meta, Channel, Version};
 
-#[cfg(not(feature = "nightly"))]
 fn main() {
-    cc::Build::new().file("src/cpuid.c").compile("libcpuid.a");
-}
+    let nightly = version_meta().unwrap().channel == Channel::Nightly;
+    let newer_than_1_27 = version().unwrap() >= Version::parse("1.27.0").unwrap();
 
-#[cfg(feature = "nightly")]
-fn main() {}
+    let use_arch = nightly || newer_than_1_27;
+
+    if use_arch {
+        println!("cargo:rustc-cfg=feature=\"nightly\"");
+    } else {
+        cc::Build::new().file("src/cpuid.c").compile("libcpuid.a");
+    }
+}
