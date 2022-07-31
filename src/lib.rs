1c3b9391300e6b9fbb222efde707298fd107dc5f
Zavier Divelbiss <semiviral@gmail.com>
2022-07-31 18:41:33 -0500

    fix platform support for fn

--- src/lib.rs
+++ src/lib.rs
@@ -573,7 +573,7 @@ impl CpuId {
     /// Extended information about topology (LEAF=0x1F).
     ///
     /// # Platforms
-    /// ❓AMD ❓Intel
+    /// ❓AMD ✅Intel
     pub fn get_extended_topology_info_v2(&self) -> Option<ExtendedTopologyIter> {
         if self.leaf_is_supported(EAX_EXTENDED_TOPOLOGY_INFO_V2) {
             Some(ExtendedTopologyIter {
