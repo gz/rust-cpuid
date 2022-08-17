54a4629002895f9ff3b022c3f183511d05acc9fd
Umio Yasuno <53935716+Umio-Yasuno@users.noreply.github.com>
2022-08-18 03:17:43 +0900

    fix support status

--- src/lib.rs
+++ src/lib.rs
@@ -455,7 +455,7 @@ impl CpuId {
     /// hierarchy.
     ///
     /// # Platforms
-    /// ✅ AMD ✅ Intel
+    /// 🟡 AMD ✅ Intel
     pub fn get_cache_parameters(&self) -> Option<CacheParametersIter> {
         if self.leaf_is_supported(EAX_CACHE_PARAMETERS) || (self.vendor == Vendor::Amd && self.leaf_is_supported(EAX_CACHE_PARAMETERS_AMD)) {
             Some(CacheParametersIter {
@@ -2506,7 +2506,7 @@ bitflags! {
 /// Yields a [CacheParameter] for each cache.
 ///
 /// # Platforms
-/// ❌ AMD ✅ Intel
+/// 🟡 AMD ✅ Intel
 #[derive(Clone)]
 #[cfg_attr(feature = "serialize", derive(Serialize, Deserialize))]
 pub struct CacheParametersIter {
